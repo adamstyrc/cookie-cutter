70398346d1624ffc8d029856c676ddc87fd1c43d
adamstyrc <styrc.adam@gmail.com>
2016-04-04 11:54:28 +0200

    Log when debug

--- biscuitlib/src/main/java/com/adamstyrc/biscuit/Logger.java
+++ biscuitlib/src/main/java/com/adamstyrc/biscuit/Logger.java
@@ -2,12 +2,15 @@ package com.adamstyrc.biscuit;
 
 import android.util.Log;
 
+
 /**
  * Created by adamstyrc on 31/03/16.
  */
 public class Logger {
 
     public static void log(String message) {
-        Log.d("Zinger", message);
+        if (BuildConfig.DEBUG) {
+            Log.d("Biscuit", message);
+        }
     }
 }
